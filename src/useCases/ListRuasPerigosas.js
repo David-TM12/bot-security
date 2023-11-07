bda884513f9f1846ee94ef6ea1db1bc8c7052849
DavidTM <deivi_gf144@hotmail.com>
2023-11-06 20:15:46 -0400

    add feature consultar ruas perigosas

--- src/useCases/ListRuasPerigosas.js
+++ src/useCases/ListRuasPerigosas.js
@@ -1,23 +1,101 @@
 import { Scenes, Markup } from 'telegraf';
-import { backButton } from '../buttons.js';
-import { getRuasPerigosas } from '../repository/db.js';
+import { backButton, buttonsMainMenu, filterButton, filterButtonInlineKeyBoard } from '../buttons.js';
+import { getRuasPerigosasPorBairro, getRuasPerigosasPorCidade, getRuasPerigosasPorUF } from '../repository/db.js';
+
+let porEstado = false;
+let porCidade = false;
+let porBairro = false;
 
 export const listRuasPerigosasScene = new Scenes.BaseScene('list_ruas_perigosas');
 listRuasPerigosasScene.enter(async ctx => {
-    
-    await ctx.reply('Infome a cidade: ');
+    await ctx.reply('Informe uma opção: ', filterButtonInlineKeyBoard);
 });
 
 listRuasPerigosasScene.leave(async ctx => {
     await ctx.reply('saindo');
     // await ctx.reply('🏃‍♂Retornando. . .', Markup.removeKeyboard());
-    // await ctx.reply('Selecione uma opção', buttonsMainMenu);
+    await ctx.reply('Selecione uma opção', buttonsMainMenu);
 });
 
-listRuasPerigosasScene.on('text', async ctx => {
-    const cidade = ctx.update.message.text;
+listRuasPerigosasScene.action(/byState/, async ctx => {
+    porEstado = true;
+    await ctx.replyWithHTML('<b>Informe a UF:</b> <i>ex.(AM)</i>');
+});
 
-    const res = await getRuasPerigosas(cidade);
-    console.log(res.length);
-    await ctx.reply(`Na cidade de ${cidade} Já houveram ${res.length} ocorrências`);
+listRuasPerigosasScene.action(/byCity/, async ctx => {
+    porCidade = true;
+    await ctx.replyWithHTML('<b>Informe a cidade:</b>');
+});
+
+listRuasPerigosasScene.action(/byDistrict/, async ctx => {
+    porBairro = true;
+    await ctx.replyWithHTML('<b>Informe o bairro:</b>');
+});
+
+
+listRuasPerigosasScene.on('text', async ctx => {
+    try {
+        if (porEstado) {
+            const uf = ctx.update.message.text;
+            const res = await getRuasPerigosasPorUF(uf);
+            if (!res || res.length === 0) {
+                throw new Error('⚠️ Não foi encontrada nenhuma ocorrência para o estado informado!!! 😉');
+            }
+            const sizeRes = res.length;
+            let a = 1;
+            for (const ocorrencia of res) {
+                if (a < sizeRes) {
+                    await ctx.replyWithHTML(`<b>UF:</b> ${ocorrencia.uf} \n<b>Cidade:</b> ${ocorrencia.cidade} \n<b>Bairro:</b> ${ocorrencia.bairro} \n<b>Rua:</b> ${ocorrencia.rua} \n<b>N° Ocorrências:</b> ${ocorrencia._count}`);
+                } else {
+                    await ctx.replyWithHTML(`<b>UF:</b> ${ocorrencia.uf} \n<b>Cidade:</b> ${ocorrencia.cidade} \n<b>Bairro:</b> ${ocorrencia.bairro} \n<b>Rua:</b> ${ocorrencia.rua} \n<b>N° Ocorrências:</b> ${ocorrencia._count}`, backButton);
+                }
+                a++;
+            }
+            porEstado = false;
+        } else if (porCidade) {
+            const cidade = ctx.update.message.text;
+            const res = await getRuasPerigosasPorCidade(cidade);
+            if (!res || res.length === 0) {
+                throw new Error('⚠️ Não foi encontrada nenhuma ocorrencia para a cidade informada!!! 😉');
+            }
+            const sizeRes = res.length;
+            console.log('a - ', sizeRes);
+            let i = 1;
+            for (const ocorrencia of res) {
+                if (i < sizeRes) {
+                    await ctx.replyWithHTML(`<b>UF:</b> ${ocorrencia.uf} <b>\nCidade:</b> ${ocorrencia.cidade} \n<b>Bairro:</b> ${ocorrencia.bairro} \n<b>Rua:</b> ${ocorrencia.rua} \n<b>N° Ocorrências:</b> ${ocorrencia._count}`);
+                } else {
+                    await ctx.replyWithHTML(`<b>UF:</b> ${ocorrencia.uf} <b>\nCidade:</b> ${ocorrencia.cidade} \n<b>Bairro:</b> ${ocorrencia.bairro} \n<b>Rua:</b> ${ocorrencia.rua} \n<b>N° Ocorrências:</b> ${ocorrencia._count}`, backButton);
+                }
+                i++;
+            }
+            porCidade = false;
+        } else if (porBairro) {
+            const bairro = ctx.update.message.text;
+            const res = await getRuasPerigosasPorBairro(bairro);
+            if (!res || res.length === 0) {
+                throw new Error('⚠️ Não foi encontrada nenhuma ocorrencia para o bairro informado!!! 😉');
+            }
+
+            const sizeRes = res.length;
+            let j = 1;
+            for (const ocorrencia of res) {
+                if (j < sizeRes) {
+                    await ctx.replyWithHTML(`<b>UF:</b> ${ocorrencia.uf} \n<b>Cidade:</b> ${ocorrencia.cidade} \n<b>Bairro:</b> ${ocorrencia.bairro} \n<b>Rua:</b> ${ocorrencia.rua} \n<b>N° Ocorrências:</b> ${ocorrencia._count}`);
+                } else {
+                    await ctx.replyWithHTML(`<b>UF:</b> ${ocorrencia.uf} \n<b>Cidade:</b> ${ocorrencia.cidade} \n<b>Bairro:</b> ${ocorrencia.bairro} \n<b>Rua:</b> ${ocorrencia.rua} \n<b>N° Ocorrências:</b> ${ocorrencia._count}`, backButton);
+                }
+                j++;
+            }
+            porBairro = false;
+        }
+
+    } catch (error) {
+        await ctx.reply(error.message, backButton);
+    }
+
+});
+
+listRuasPerigosasScene.action(/voltar/, async ctx => {
+    await ctx.scene.leave();
 });
